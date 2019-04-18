ab2b8964dd1aff026d1ac1bc07fe39523f3f002e
Garrett W <garrett@gumgum.com>
2019-04-18 16:04:11 -0700

    fixed the google isue now more issues

--- src/providers/UserProvider.jsx
+++ src/providers/UserProvider.jsx
@@ -4,22 +4,17 @@ import { auth, createUserProfileDocument } from "../firebase";
 export const UserContext = createContext({ user: null });
 
 class UserProvider extends Component {
-  state = {
-    user: null
-  };
+  state = { user: null };
 
   unsubscribeFromAuth = null;
 
   componentDidMount = async () => {
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if (userAuth) {
-        const userRef = await createUserProfileDocument(userAuth);
-        userRef.onSnapshot(snapshot => {
-          this.setState({ user: { uid: snapshot.id, ...snapshot.data() } });
-        });
+        const user = await createUserProfileDocument(userAuth);
+        return this.setState({ user });
       }
-
-      this.setState({ user: userAuth });
+      this.setState({ user: null });
     });
   };
 
@@ -28,8 +23,8 @@ class UserProvider extends Component {
   };
 
   render() {
-    const { user } = this.state;
     const { children } = this.props;
+    const { user } = this.state;
 
     return <UserContext.Provider value={user}>{children}</UserContext.Provider>;
   }
